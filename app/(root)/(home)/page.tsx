9c78bfefcdffd3c2c8496db5f4444f00740a12d0
Shahirul <shahiruliimalo@gmail.com>
2024-09-30 22:15:13 +0800

    styles:change styles

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="text-[30px] tracking-tighter font-extrabold text-purple-950">
+    <div className="text-[30px] tracking-tighter font-extrabold mt-[-35px] ml-[-20px] text-purple-950">
       Home
     </div>
   );
--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -10,8 +10,8 @@ import React from "react";
 const LeftSidebar = () => {
   const pathname = usePathname();
   return (
-    <section className="sticky left-0 top-0 flex h-screen items-center justify-between p-6 w-fit flex-col text-black bg-[#A594F9] max-sm:hidden lg:w-[264px]">
-      <div className="flex flex-1 flex-col justify-center items-center min-h-screen gap-6">
+    <section className="sticky left-0 top-0 flex h-screen items-center justify-between p-6 w-fit flex-col text-black bg-[#A594F9] max-sm:hidden lg:w-[264px] overflow-y-hidden">
+      <div className="flex flex-1 flex-col justify-start items-center mt-[100px] min-h-screen gap-6 ">
         {sidebarLinks.map((item) => {
           const isActive =
             pathname === item.route || pathname.startsWith(`${item.route}/`);
