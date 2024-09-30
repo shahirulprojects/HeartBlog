3d4d3c0197b06090be07cdac4ffeabb7be2d4421
Shahirul <shahiruliimalo@gmail.com>
2024-09-30 18:45:17 +0800

    fix:fix styles for navbar and mobilenav

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -29,11 +29,11 @@ const MobileNav = () => {
         </SheetTrigger>
         <SheetContent side="left" className="border-none bg-[#A594F9]">
           <Link href="/" className="flex items-center gap-1">
-            <div className="gap-3 flex items-center justify-center">
+            <div className="gap-2 flex items-center justify-center">
               <Heart
                 width={50}
                 height={50}
-                className="max-sm:size-20 text-white"
+                className="max-sm:size-18 text-white fill-white"
               />
               <p className="text-[35px] text-white font-thin tracking-tighter">
                 Heart Blog
@@ -42,28 +42,30 @@ const MobileNav = () => {
           </Link>
           <div className="flex h-[calc(100vh-72px)] flex-col justify-between overflow-y-auto">
             <SheetClose asChild>
-              <section className="flex h-full flex-col gap-6 pt-6 text-white">
+              <section className="flex h-full flex-col gap-6 pt-6 text-black">
                 {sidebarLinks.map((item) => {
                   const isActive = pathname === item.route;
                   return (
                     <SheetClose asChild key={item.route}>
-                      <Link
-                        href={item.route}
-                        key={item.label}
-                        className={cn(
-                          "flex gap-4 items-center p-4 rounded-lg justify-start",
-                          { "bg-blue-200 text-white": isActive }
-                        )}
-                      >
-                        <Image
-                          src={item.imgUrl}
-                          alt={item.label}
-                          width={20}
-                          height={20}
-                          className={isActive ? "invert" : ""}
-                        />
-                      </Link>
-                      <p className="font-semibold">{item.label}</p>
+                      <div className="flex items-center justify-center">
+                        <Link
+                          href={item.route}
+                          key={item.label}
+                          className={cn(
+                            "flex gap-4 items-center w-full p-4 rounded-lg justify-start",
+                            { "bg-blue-200 text-white": isActive }
+                          )}
+                        >
+                          <Image
+                            src={item.imgUrl}
+                            alt={item.label}
+                            width={20}
+                            height={20}
+                            className={isActive ? "invert" : ""}
+                          />
+                          <p className="font-semibold">{item.label}</p>
+                        </Link>
+                      </div>
                     </SheetClose>
                   );
                 })}
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,11 @@ const Navbar = () => {
   return (
     <nav className="flex-between fixed z-50 w-full px-6 py-4 lg:px-10 bg-[#A594F9]">
       <Link href="/" className="flex items-center gap-1">
-        <Heart width={50} height={50} className="max-sm:hidden text-white" />
+        <Heart
+          width={50}
+          height={50}
+          className="max-sm:hidden text-white fill-white"
+        />
         <div className="flex items-center justify-center">
           <p className="text-[35px] text-white max-sm:hidden font-thin tracking-tighter">
             Heart Blog
