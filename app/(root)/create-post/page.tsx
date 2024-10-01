ee67599a230796313501d6cae7a229ccc15b7cdf
Shahirul <shahiruliimalo@gmail.com>
2024-10-01 15:55:17 +0800

    feat:implement form

--- app/(root)/create-post/page.tsx
+++ app/(root)/create-post/page.tsx
@@ -1,9 +1,75 @@
+"use client";
 import React from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 
 const CreatePost = () => {
+  const formSchema = z.object({
+    username: z.string().min(2, {
+      message: "Username must be at least 2 characters.",
+    }),
+  });
+
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+  }
+
   return (
-    <div className="text-[30px] tracking-tighter font-extrabold mt-[-35px] ml-[-20px] text-purple-950">
-      Create Post
+    <div className="text-[30px] mt-[-35px] ml-[-20px]">
+      <h1 className="font-extrabold tracking-tighter text-purple-950">
+        Create Post
+      </h1>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="flex w-full flex-col">
+                <FormLabel className="paragraph-semibold text-dark400_light800">
+                  Question Title{" "}
+                  <span className="text-red-500 dark:text-red-300">*</span>
+                </FormLabel>
+                <FormControl className="mt-3.5">
+                  <Input
+                    className="no-focus paragraph-regular background-light900_dark300 light-border-2 text-dark300_light700 min-h-[56px] border"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription className="body-regular mt-2.5 text-gray-500 dark:text-light-500">
+                  Insert a specific title for the problem.
+                </FormDescription>
+                {/* FormMessage is for displaying success or error messages,in this case we want to display error message */}
+                <FormMessage className="text-red-500 dark:text-red-300" />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </div>
   );
 };
